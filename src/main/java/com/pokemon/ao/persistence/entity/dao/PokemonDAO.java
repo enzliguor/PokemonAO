759b6121d23f8b536396995f3e8535b9a5416a9e
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 13:12:21 +0200

    feat(PokemonDAO): Adds the PokemonDAO class

---
+++ src/main/java/com/pokemon/ao/persistence/entity/dao/PokemonDAO.java
@@ -0,0 +1,9 @@
+package com.pokemon.ao.persistence.entity.dao;
+
+import com.pokemon.ao.persistence.entity.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface PokemonDAO extends JpaRepository<Pokemon, Long> {
+
+}

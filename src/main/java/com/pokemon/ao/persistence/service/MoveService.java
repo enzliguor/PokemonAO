d4007b2a358c76861d53b8dcd90ccfb2348e7e2f
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 16:23:12 +0200

    refactor(AbstractService): adds the Abstract Service class that defines the default behaviors of a Service, and from which extend all other services

--- src/main/java/com/pokemon/ao/persistence/service/MoveService.java
+++ src/main/java/com/pokemon/ao/persistence/service/MoveService.java
@@ -1,33 +1,14 @@
 package com.pokemon.ao.persistence.service;
 
 import com.pokemon.ao.domain.MoveVO;
-import com.pokemon.ao.persistence.dao.MoveDAO;
 import com.pokemon.ao.persistence.entity.Move;
-import com.pokemon.ao.persistence.marshaller.MoveMarshaller;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pokemon.ao.persistence.marshaller.Marshaller;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MoveService {
-    private final MoveDAO moveDao;
-
-    private final MoveMarshaller moveMarshaller;
-
-    @Autowired
-    public MoveService(MoveDAO moveDao , MoveMarshaller moveMarshaller) {
-        this.moveDao = moveDao;
-        this.moveMarshaller = moveMarshaller;
-    }
-
-    public Move getMoveById(Long id){
-        return moveDao.findById(id).orElse(null);
-    }
-
-    public Move saveMoveVO(MoveVO moveVO){
-        return moveDao.save(moveMarshaller.marshall(moveVO));
-    }
-
-    public void deleteMove(Long id){
-        moveDao.deleteById(id);
+public class MoveService extends AbstractService<MoveVO, Move, Long> {
+    protected MoveService(Marshaller<MoveVO, Move> marshaller, JpaRepository<Move, Long> dao) {
+        super(marshaller, dao);
     }
 }
--- src/main/java/com/pokemon/ao/persistence/service/PokemonService.java
+++ src/main/java/com/pokemon/ao/persistence/service/PokemonService.java
@@ -1,33 +1,14 @@
 package com.pokemon.ao.persistence.service;
 
 import com.pokemon.ao.domain.PokemonVO;
-import com.pokemon.ao.persistence.dao.PokemonDAO;
 import com.pokemon.ao.persistence.entity.Pokemon;
-import com.pokemon.ao.persistence.marshaller.PokemonMarshaller;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pokemon.ao.persistence.marshaller.Marshaller;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PokemonService {
-    private final PokemonDAO pokemonDao;
-
-    private final PokemonMarshaller pokemonMarshaller;
-
-    @Autowired
-    public PokemonService(PokemonDAO pokemonDao , PokemonMarshaller pokemonMarshaller) {
-        this.pokemonDao = pokemonDao;
-        this.pokemonMarshaller = pokemonMarshaller;
-    }
-
-    public Pokemon getPokemonById(Long id){
-        return pokemonDao.findById(id).orElse(null);
-    }
-
-    public Pokemon savePokemonVO(PokemonVO pokemonVO){
-        return pokemonDao.save(pokemonMarshaller.marshall(pokemonVO));
-    }
-
-    public void deletePokemon(Long id){
-        pokemonDao.deleteById(id);
+public class PokemonService extends AbstractService<PokemonVO, Pokemon, Long>{
+    protected PokemonService(Marshaller<PokemonVO, Pokemon> marshaller, JpaRepository<Pokemon, Long> dao) {
+        super(marshaller, dao);
     }
 }

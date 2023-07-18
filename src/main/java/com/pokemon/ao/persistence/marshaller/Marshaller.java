67c43fe56c9fc9a12728a7b9df95b7cc2edd3b7b
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 16:19:32 +0200

    refactor(Marshaller): adds restrictions on Generics used in the Marshaller interface

--- src/main/java/com/pokemon/ao/persistence/marshaller/Marshaller.java
+++ src/main/java/com/pokemon/ao/persistence/marshaller/Marshaller.java
@@ -1,6 +1,9 @@
 package com.pokemon.ao.persistence.marshaller;
 
-public interface Marshaller<V, E> {
+import com.pokemon.ao.domain.ValueObject;
+import com.pokemon.ao.persistence.entity.EntityDB;
+
+public interface Marshaller<V extends ValueObject, E extends EntityDB> {
     E marshall(V v);
     V unmarshall(E e);
 }

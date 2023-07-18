e272fca60d474b794287277b15abf0628911395b
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 13:11:38 +0200

    feat(MoveDAO): Adds the MoveDAO class

---
+++ src/main/java/com/pokemon/ao/persistence/entity/dao/MoveDAO.java
@@ -0,0 +1,10 @@
+package com.pokemon.ao.persistence.entity.dao;
+
+
+import com.pokemon.ao.persistence.entity.Move;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MoveDAO extends JpaRepository<Move, Long> {
+
+}

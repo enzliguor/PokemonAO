6d1f25ed81eaebf5e0235a8fd0b6851c2d381b91
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 16:15:22 +0200

    fix(dao): deletes from all dao useless import and method declarations

--- src/main/java/com/pokemon/ao/persistence/dao/MoveDAO.java
+++ src/main/java/com/pokemon/ao/persistence/dao/MoveDAO.java
@@ -3,13 +3,7 @@ package com.pokemon.ao.persistence.dao;
 import com.pokemon.ao.persistence.entity.Move;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface MoveDAO extends JpaRepository<Move, Long> {
-
-    Optional<Move> findById(Long id);
-
-    Move findByName(String name);
-
 }
--- src/main/java/com/pokemon/ao/persistence/dao/PokemonDAO.java
+++ src/main/java/com/pokemon/ao/persistence/dao/PokemonDAO.java
@@ -3,12 +3,7 @@ package com.pokemon.ao.persistence.dao;
 import com.pokemon.ao.persistence.entity.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface PokemonDAO extends JpaRepository<Pokemon, Long> {
-
-    Optional<Pokemon> findById(Long id);
-
-    Pokemon findByName(String name);
 }

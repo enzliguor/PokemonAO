dc15b3c519049b24797587b8ddf50ce9ac3ba850
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-18 12:22:46 +0200

    init commit

---
+++ src/main/java/com/pokemon/ao/PokemonAoApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.ao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonAoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemon/ao/PokemonAoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.ao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

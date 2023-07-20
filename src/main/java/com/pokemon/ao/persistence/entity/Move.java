55b1fe9e6944e39310ea9e55059d59f31af3131e
plabriola <paololabriola02@gmail.com>
2023-07-20 17:53:27 +0200

    refactor(Move): removes @GeneratedValue annotation to allow moves to be saved with given id

--- src/main/java/com/pokemon/ao/persistence/entity/Move.java
+++ src/main/java/com/pokemon/ao/persistence/entity/Move.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 public class Move implements EntityDB {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")

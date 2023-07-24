0b664a68ee32487448363337e876ca39c4508e55
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-24 18:33:41 +0200

    feat(PokemonApiClient): adds the methods that handle the retrieval of a number of Move from the API

--- src/main/java/com/pokemon/ao/api/PokemonApiClient.java
+++ src/main/java/com/pokemon/ao/api/PokemonApiClient.java
@@ -1,5 +1,7 @@
 package com.pokemon.ao.api;
 
+import com.pokemon.ao.domain.TypeVO;
+import com.pokemon.ao.dto.MoveDTO;
 import com.pokemon.ao.dto.SpeciesDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +55,42 @@ public class PokemonApiClient {
         Map<String, Object> typeObj = types.get(0);
         Map<String, Object> typeMap = (Map<String, Object>) typeObj.get("type");
         return (String) typeMap.get("name");
+    }
+
+    public Set<MoveDTO> getTypesMoves(List<TypeVO> typeVO, int maxMoves) {
+        int maxMovesPerType = (maxMoves + typeVO.size() - 1) / typeVO.size();
+        return (Set<MoveDTO>) typeVO.stream()
+                .parallel()
+                .map(type -> invokeApi("https://pokeapi.co/api/v2/type/{name}", Map.class, type.getName()))
+                .filter(Objects::nonNull)
+                .flatMap(jsonResponse -> this.getMoves((List) jsonResponse.get("moves"), maxMovesPerType).stream())
+                .limit(maxMoves)
+                .collect(Collectors.toSet());
+    }
+
+    private Set<MoveDTO> getMoves(List<Map<String, String>> moves, int maxMovesPerType) {
+        return moves.stream()
+                .limit(maxMovesPerType)
+                .map(moveMap -> moveMap.get("name"))
+                .map(this::getMoveByName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+    }
+
+    public MoveDTO getMoveByName(String moveName) {
+        Map<String, Object> jsonResponse = this.invokeApi("https://pokeapi.co/api/v2/move/{name}", Map.class, moveName);
+        if (jsonResponse == null) return null;
+
+        Map<String, Object> typeObj = (Map<String, Object>) jsonResponse.get("type");
+        String typeName = (String) typeObj.get("name");
+
+        int power = (jsonResponse.get("power") != null) ? Integer.parseInt(jsonResponse.get("power").toString()) : 0;
+
+        return MoveDTO.builder()
+                .name(moveName)
+                .power(power)
+                .typeName(typeName)
+                .build();
     }
 
     private <T> T invokeApi(String url, Class<T> responseType, Object... urlVariables) {

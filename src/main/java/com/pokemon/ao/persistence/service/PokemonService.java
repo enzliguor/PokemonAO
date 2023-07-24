8042106b304c43bec6c21525868cb4dd5b1727b9
nmessuti <nicola.messuti@alten.it>
2023-07-24 10:31:01 +0200

    refactor(PokemonService.java): remove unused local field

--- src/main/java/com/pokemon/ao/persistence/service/PokemonService.java
+++ src/main/java/com/pokemon/ao/persistence/service/PokemonService.java
@@ -4,7 +4,6 @@ import com.pokemon.ao.domain.PokemonVO;
 import com.pokemon.ao.persistence.dao.PokemonDAO;
 import com.pokemon.ao.persistence.entity.Pokemon;
 import com.pokemon.ao.persistence.marshaller.Marshaller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +11,12 @@ import java.util.List;
 
 @Service
 public class PokemonService extends AbstractService<PokemonVO, Pokemon, Long> {
-    private final PokemonDAO pokemonDAO;
-    @Autowired
-    protected PokemonService(Marshaller<PokemonVO, Pokemon> marshaller, JpaRepository<Pokemon, Long> dao, PokemonDAO pokemonDAO) {
+    protected PokemonService(Marshaller<PokemonVO, Pokemon> marshaller, JpaRepository<Pokemon, Long> dao) {
         super(marshaller, dao);
-        this.pokemonDAO = pokemonDAO;
     }
 
     public List<Long> findAllIds(){
+        PokemonDAO pokemonDAO = (PokemonDAO) dao;
         return pokemonDAO.findAllIds();
     }
 }

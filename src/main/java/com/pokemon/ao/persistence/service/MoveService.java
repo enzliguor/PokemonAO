d105ca1c9bee07208afa2f4c85c296162ce90c1d
VitaCha <vitaliychayka10@gmail.com>
2023-07-24 11:52:42 +0200

    feat(MoveService):adds mapMovesIdsToMoveVO method

--- src/main/java/com/pokemon/ao/persistence/service/MoveService.java
+++ src/main/java/com/pokemon/ao/persistence/service/MoveService.java
@@ -7,10 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class MoveService extends AbstractService<MoveVO, Move, Integer> {
     @Autowired
     protected MoveService(Marshaller<MoveVO, Move> marshaller, JpaRepository<Move, Integer> dao) {
         super(marshaller, dao);
+    }
+
+    public Set<MoveVO> mapMovesIdsToMoveVO(Set<Integer> movesId ) {
+        return movesId.stream ()
+                .map(moveId -> this.findById(moveId))
+                .collect(Collectors.toSet());
     }
 }

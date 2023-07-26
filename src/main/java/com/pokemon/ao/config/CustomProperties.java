a4190994249782fbc0832e0fa5182e390b9faeff
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-07-26 17:08:28 +0200

    refactor(customProperties): does a refactoring on the names of some properties and adapts accordingly to the CustomProperties class

--- src/main/java/com/pokemon/ao/config/CustomProperties.java
+++ src/main/java/com/pokemon/ao/config/CustomProperties.java
@@ -1,31 +1,27 @@
 package com.pokemon.ao.config;
 
 import com.pokemon.ao.domain.PokemonVO;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Map;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomProperties {
-    private PokemonVO pokemon;
-
-    private Map<String, String> icons;
-
+    private PokemonVO defaultPokemon;
+    private Map<String, String> typeIcons;
     private Integer speciesCount;
-
     private Integer movesCount;
-  
     private int teamSize;
-
-    private String urlPokemonDajeExchange;
-
-    private String urlPokemonDajeStatusExchange;
-
+    private String pokemonDajeExchangeUrl;
+    private String pokemonDajeStatusExchangeUrl;
     private int maxMovesPerPokemon;
-
     private int minMovesPerPokemon;
+    private String pokemonApiSpeciesUrl;
+    private String pokemonApiMovesUrl;
+    private String pokemonApiTypesUrl;
 
 }

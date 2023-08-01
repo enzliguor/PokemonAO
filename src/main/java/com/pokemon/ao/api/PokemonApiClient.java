3cf2bb419a734b13826db5d263e7a0e4a6d99b57
mcilli <marianna.cilli@gmail.com>
2023-08-01 10:30:37 +0200

    Refactor (PokemonApiClient): changed the methods getSpecies, getTypeMoves and getMoves with a LinkedHashSet implementation instead the Set implementation.

--- src/main/java/com/pokemon/ao/api/PokemonApiClient.java
+++ src/main/java/com/pokemon/ao/api/PokemonApiClient.java
@@ -12,10 +12,7 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -30,17 +27,37 @@ public class PokemonApiClient {
         this.restTemplate = restTemplate;
         this.customProperties = customProperties;
     }
-
     public Set<SpeciesDTO> getSpecies(int numberOfSpecies) {
-        return IntStream.rangeClosed(1, numberOfSpecies)
-                .mapToObj(speciesID -> invokeApi(customProperties.getPokemonApiSpeciesUrl(), Map.class, speciesID))
+        return    IntStream.rangeClosed(1, numberOfSpecies)
+                .mapToObj(speciesID -> invokeApi("https://pokeapi.co/api/v2/pokemon/{id}", Map.class, speciesID))
                 .filter(Objects::nonNull)
                 .map(jsonResponse -> SpeciesDTO.builder()
                         .name((String) jsonResponse.get("name"))
                         .spriteUrl(this.getFrontDefaultSprite((Map) jsonResponse.get("sprites")))
                         .type(this.getTypeName((List) jsonResponse.get("types")))
                         .build())
-                .collect(Collectors.toSet());
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    public Set<MoveDTO> getTypesMoves(List<TypeVO> typeVO, int maxMoves) {
+        int maxMovesPerType = (maxMoves + typeVO.size() - 1) / typeVO.size();
+        return (Set<MoveDTO>) typeVO.stream()
+                .parallel()
+                .map(type -> invokeApi("https://pokeapi.co/api/v2/type/{name}", Map.class, type.getName()))
+                .filter(Objects::nonNull)
+                .flatMap(jsonResponse -> this.getMoves((List) jsonResponse.get("moves"), maxMovesPerType).stream())
+                .limit(maxMoves)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    private Set<MoveDTO> getMoves(List<Map<String, String>> moves, int maxMovesPerType) {
+        return  moves.stream()
+                .limit(maxMovesPerType)
+                .map(moveMap -> moveMap.get("name"))
+                .map(this::getMoveByName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
     }
 
     private String getFrontDefaultSprite(Map<String, Map<String, Map<String, String>>> sprites) {
@@ -58,26 +75,6 @@ public class PokemonApiClient {
         Map<String, Object> typeObj = types.get(0);
         Map<String, Object> typeMap = (Map<String, Object>) typeObj.get("type");
         return (String) typeMap.get("name");
-    }
-
-    public Set<MoveDTO> getTypesMoves(List<TypeVO> typeVO, int maxMoves) {
-        int maxMovesPerType = (maxMoves + typeVO.size() - 1) / typeVO.size();
-        return (Set<MoveDTO>) typeVO.stream()
-                .parallel()
-                .map(type -> invokeApi(customProperties.getPokemonApiTypesUrl(), Map.class, type.getName()))
-                .filter(Objects::nonNull)
-                .flatMap(jsonResponse -> this.getMoves((List) jsonResponse.get("moves"), maxMovesPerType).stream())
-                .limit(maxMoves)
-                .collect(Collectors.toSet());
-    }
-
-    private Set<MoveDTO> getMoves(List<Map<String, String>> moves, int maxMovesPerType) {
-        return moves.stream()
-                .limit(maxMovesPerType)
-                .map(moveMap -> moveMap.get("name"))
-                .map(this::getMoveByName)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
     }
 
     public MoveDTO getMoveByName(String moveName) {

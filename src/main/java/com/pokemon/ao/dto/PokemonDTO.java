17756554e1c2ce910a3f207e3dddb582c3ead54e
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-08-02 18:32:19 +0200

    fix(PokemonDTO): fixes the way this class is converted to JSON

--- src/main/java/com/pokemon/ao/dto/PokemonDTO.java
+++ src/main/java/com/pokemon/ao/dto/PokemonDTO.java
@@ -1,44 +1,29 @@
 package com.pokemon.ao.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
 import java.util.Set;
 
 @Builder
 @Getter
 @EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PokemonDTO implements DTO {
-
+    @JsonProperty("id")
     private Integer speciesId;
 
     private String name;
-
+    @JsonProperty("current_hp")
     private int currentHp;
-
+    @JsonProperty("max_hp")
     private int maxHp;
-
+    @JsonProperty("type")
     private Integer typeId;
-
+    @JsonProperty("moves")
     private Set<Integer> movesIds;
-
+    @JsonProperty("original_trainer")
     private String originalTrainer;
 
-
-    @JsonCreator
-    private PokemonDTO(@JsonProperty("id") Integer speciesId,
-                      @JsonProperty("name") String name,
-                      @JsonProperty("current_hp") int currentHp,
-                      @JsonProperty("max_hp") int maxHp,
-                      @JsonProperty("type") Integer typeId,
-                      @JsonProperty("moves") Set<Integer> movesIds,
-                      @JsonProperty("original_trainer") String originalTrainer) {
-        this.speciesId = speciesId;
-        this.name = name;
-        this.currentHp = currentHp;
-        this.maxHp = maxHp;
-        this.typeId = typeId;
-        this.movesIds = movesIds;
-        this.originalTrainer = originalTrainer;
-    }
 }

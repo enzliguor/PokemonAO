ea7d676cc0d03e613d897e4dd259a20d2b4e81b1
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-08-08 15:46:35 +0200

    refactor(PokemonController): deletes the PokemonExchangeController class by transferring the management of the 'exchange/{pokemonId}' endpoint to the PokemonController

--- src/main/java/com/pokemon/ao/controller/PokemonController.java
+++ src/main/java/com/pokemon/ao/controller/PokemonController.java
@@ -2,14 +2,12 @@ package com.pokemon.ao.controller;
 
 import com.pokemon.ao.config.CustomProperties;
 import com.pokemon.ao.domain.PokemonVO;
+import com.pokemon.ao.utility.ExchangePokemonHandler;
 import com.pokemon.ao.utility.PokemonUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +17,13 @@ import java.util.List;
 public class PokemonController {
 
     private final PokemonUtility pokemonUtility;
+    private final ExchangePokemonHandler exchangePokemonService;
     private final int teamSize;
 
     @Autowired
-    private PokemonController(PokemonUtility pokemonUtility, CustomProperties customProperties) {
+    private PokemonController(PokemonUtility pokemonUtility, ExchangePokemonHandler exchangePokemonService, CustomProperties customProperties) {
         this.pokemonUtility = pokemonUtility;
+        this.exchangePokemonService = exchangePokemonService;
         this.teamSize = customProperties.getTeamSize();
 
     }
@@ -32,5 +32,13 @@ public class PokemonController {
     public ResponseEntity<List<PokemonVO>> getRandomTeam() {
         List<PokemonVO> randomTeam = pokemonUtility.getRandomPokemon(teamSize);
         return new ResponseEntity<>(randomTeam, HttpStatus.OK);
+    }
+
+    @PostMapping("/exchange/{pokemonID}")
+    public ResponseEntity<PokemonVO> exchangePokemon(@PathVariable Integer pokemonID) {
+        PokemonVO receivedPokemon = this.exchangePokemonService.exchange(pokemonID);
+        if(receivedPokemon == null)  return ResponseEntity.internalServerError().build();
+
+        return ResponseEntity.ok(receivedPokemon);
     }
 }

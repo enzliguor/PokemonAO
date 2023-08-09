9337ed43e31f5044a3a407367a3dd26fbecbc8f7
plabriola <paololabriola02@gmail.com>
2023-08-09 12:38:37 +0200

    refactor(PokemonApiClient): Changes the handling of API calls from RestTemplate to webclient and transfers the responsibility for deserializing json responses of API calls to the new deserialization classes.

--- src/main/java/com/pokemon/ao/api/PokemonApiClient.java
+++ src/main/java/com/pokemon/ao/api/PokemonApiClient.java
@@ -4,14 +4,13 @@ import com.pokemon.ao.config.CustomProperties;
 import com.pokemon.ao.domain.TypeVO;
 import com.pokemon.ao.dto.MoveDTO;
 import com.pokemon.ao.dto.SpeciesDTO;
+import com.pokemon.ao.dto.TypeDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import reactor.core.publisher.Mono;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,96 +18,64 @@ import java.util.stream.IntStream;
 @Component
 @Slf4j
 public class PokemonApiClient {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
+
+    private final String pokemonApiBaseUrl;
+
     private final CustomProperties customProperties;
 
     @Autowired
-    private PokemonApiClient(RestTemplate restTemplate, CustomProperties customProperties) {
-        this.restTemplate = restTemplate;
+    private PokemonApiClient(CustomProperties customProperties) {
         this.customProperties = customProperties;
+        this.pokemonApiBaseUrl = this.customProperties.getPokemonApiBaseUrl();
+        this.webClient = WebClient.builder()
+                .baseUrl(pokemonApiBaseUrl)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
+                .build();
     }
+
     public Set<SpeciesDTO> getSpecies(int numberOfSpecies) {
-        return    IntStream.rangeClosed(1, numberOfSpecies)
+        String pokemonApiSpeciesUrl = this.customProperties.getPokemonApiSpeciesUrl();
+        return IntStream.rangeClosed(1, numberOfSpecies)
                 .parallel()
-                .mapToObj(speciesID -> invokeApi("https://pokeapi.co/api/v2/pokemon/{id}", Map.class, speciesID))
+                .mapToObj(speciesID -> invokeApi(pokemonApiSpeciesUrl, SpeciesDTO.class, speciesID))
                 .filter(Objects::nonNull)
-                .map(jsonResponse -> SpeciesDTO.builder()
-                        .name((String) jsonResponse.get("name"))
-                        .spriteUrl(this.getFrontDefaultSprite((Map) jsonResponse.get("sprites")))
-                        .type(this.getTypeName((List) jsonResponse.get("types")))
-                        .build())
                 .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     public Set<MoveDTO> getTypesMoves(List<TypeVO> typeVO, int maxMoves) {
+        String pokemonApiTypeUrl = this.customProperties.getPokemonApiTypesUrl();
         int maxMovesPerType = (maxMoves + typeVO.size() - 1) / typeVO.size();
-        return (Set<MoveDTO>) typeVO.stream()
+        return typeVO.stream()
                 .parallel()
-                .map(type -> invokeApi("https://pokeapi.co/api/v2/type/{name}", Map.class, type.getName()))
+                .map(type -> invokeApi(pokemonApiTypeUrl, TypeDTO.class, type.getName()))
                 .filter(Objects::nonNull)
-                .flatMap(jsonResponse -> this.getMoves((List) jsonResponse.get("moves"), maxMovesPerType).stream())
+                .flatMap(typeDTO -> this.getMoves(typeDTO.getMovesName(), maxMovesPerType).stream())
                 .limit(maxMoves)
                 .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
-    private Set<MoveDTO> getMoves(List<Map<String, String>> moves, int maxMovesPerType) {
-        return  moves.stream()
+    private Set<MoveDTO> getMoves(Set<String> movesName, int maxMovesPerType) {
+        String pokemonApiMoveUrl = this.customProperties.getPokemonApiMovesUrl();
+        return movesName.stream()
                 .limit(maxMovesPerType)
-                .map(moveMap -> moveMap.get("name"))
-                .map(this::getMoveByName)
+                .map(name -> this.invokeApi(pokemonApiMoveUrl, MoveDTO.class, name))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toCollection(LinkedHashSet::new));
 
     }
 
-    private String getFrontDefaultSprite(Map<String, Map<String, Map<String, String>>> sprites) {
-        if (sprites == null) return null;
-        return sprites.get("other")
-                .get("home")
-                .get("front_default");
-    }
-
-
-    private String getTypeName(List<Map<String, Object>> types) {
-        if (types == null || types.isEmpty()) {
-            return null;
-        }
-        Map<String, Object> typeObj = types.get(0);
-        Map<String, Object> typeMap = (Map<String, Object>) typeObj.get("type");
-        return (String) typeMap.get("name");
-    }
-
-    public MoveDTO getMoveByName(String moveName) {
-        Map<String, Object> jsonResponse = this.invokeApi(customProperties.getPokemonApiMovesUrl(), Map.class, moveName);
-        if (jsonResponse == null) return null;
-
-        Map<String, Object> typeObj = (Map<String, Object>) jsonResponse.get("type");
-        String typeName = (String) typeObj.get("name");
-
-        int power = (jsonResponse.get("power") != null) ? Integer.parseInt(jsonResponse.get("power").toString()) : 0;
-
-        return MoveDTO.builder()
-                .name(moveName)
-                .power(power)
-                .typeName(typeName)
-                .build();
-    }
-
     private <T> T invokeApi(String url, Class<T> responseType, Object... urlVariables) {
-        try {
-            String constructedUrl = UriComponentsBuilder.fromUriString(url).buildAndExpand(urlVariables).toUriString();
-            ResponseEntity<T> response = restTemplate.getForEntity(constructedUrl, responseType);
-            log.info("Calling {}", constructedUrl);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                return response.getBody();
-            } else {
-                String errorMessage = "Error calling this URL: " + url + " Status Code: " + response.getStatusCode();
-                log.error(errorMessage);
-            }
-        } catch (RestClientException e) {
-            String errorMessage = "Error calling this URL: " + url;
-            log.error(errorMessage, e);
-        }
-        return null;
+        String constructedUrl = UriComponentsBuilder.fromUriString(url).buildAndExpand(urlVariables).toUriString();
+        log.info("Calling {}", this.pokemonApiBaseUrl + constructedUrl);
+        Mono<T> responseMono = webClient.get()
+                .uri(constructedUrl)
+                .retrieve()
+                .bodyToMono(responseType)
+                .onErrorResume(throwable -> {
+                    log.error("Error calling this URL: {}", this.pokemonApiBaseUrl + constructedUrl, throwable);
+                    return Mono.empty();
+                });
+        return responseMono.block();
     }
 }

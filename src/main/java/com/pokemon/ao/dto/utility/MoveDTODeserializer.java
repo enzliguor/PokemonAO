4e105b312df55ef22dd6d51207e709907f1662b4
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-08-09 12:44:29 +0200

    refactor: adds @NoArgsConstructor annotation to these classes with a private access

--- src/main/java/com/pokemon/ao/dto/utility/MoveDTODeserializer.java
+++ src/main/java/com/pokemon/ao/dto/utility/MoveDTODeserializer.java
@@ -1,20 +1,21 @@
 package com.pokemon.ao.dto.utility;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokemon.ao.dto.MoveDTO;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.io.IOException;
-
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MoveDTODeserializer extends JsonDeserializer<MoveDTO> {
     @Override
-    public MoveDTO deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+    public MoveDTO deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(p);
+        JsonNode jsonNode = objectMapper.readTree(jsonParser);
 
         String moveName = jsonNode.get("name").asText();
         String moveTypeName = jsonNode.get("type").get("name").asText();
--- src/main/java/com/pokemon/ao/dto/utility/TypeDTODeserializer.java
+++ src/main/java/com/pokemon/ao/dto/utility/TypeDTODeserializer.java
@@ -1,22 +1,23 @@
 package com.pokemon.ao.dto.utility;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokemon.ao.dto.TypeDTO;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TypeDTODeserializer extends JsonDeserializer<TypeDTO> {
     @Override
-    public TypeDTO deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+    public TypeDTO deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(p);
+        JsonNode jsonNode = objectMapper.readTree(jsonParser);
 
         String typeName = jsonNode.get("name").asText();
         Set<String> movesName = new LinkedHashSet<>();

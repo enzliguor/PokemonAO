6a9d35aebefb1e830f29f20c0e4a9dfcd2972bc4
plabriola <paololabriola02@gmail.com>
2023-08-09 12:31:15 +0200

    refactor(MoveDTO): adds @JsonDeserialize annotation to specify the custom deserialization class MoveDTODeserializer

--- src/main/java/com/pokemon/ao/dto/MoveDTO.java
+++ src/main/java/com/pokemon/ao/dto/MoveDTO.java
@@ -1,11 +1,14 @@
 package com.pokemon.ao.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.pokemon.ao.dto.utility.MoveDTODeserializer;
 import lombok.*;
 
 @Builder
 @Getter
 @EqualsAndHashCode
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonDeserialize(using = MoveDTODeserializer.class)
 public class MoveDTO implements DTO {
 
     private String name;
